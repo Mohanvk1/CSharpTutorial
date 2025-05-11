a81d3488f82058389ccf3e31c8457eb31762d3c1
Mohanvk1 <mohan07.ade@gmail.com>
2025-05-10 21:11:04 -0500

    Praticing CSharp session

---
+++ Datatypes/C_Datatypes.cs
@@ -0,0 +1,9 @@
+internal class C_Datatypes
+{
+    public static int X( )
+    {
+      
+    int myNum = 6;  
+    return     myNum;      
+    }     
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,103 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+
+
+    private static void Main(string[] args)
+    {
+        
+      Console.WriteLine("Hello World");
+      int myNum=5;
+      Console.WriteLine(myNum);
+
+      double x=0.55;
+      Console.WriteLine(x);
+
+      float y=2.66f;
+      int z=(int)y;
+      Console.WriteLine(z);
+
+      string userName="mohan";
+      Console.WriteLine(userName);
+
+Console.WriteLine("Enter a Name:");
+ string? Name=Console.ReadLine();
+      Console.WriteLine(Name);
+
+int a=50+20;
+Console.WriteLine(a);
+
+
+int b=Math.Min(5,10);
+Console.WriteLine(b);
+
+string Greeting="Hello World!";
+Console.WriteLine(Greeting);
+
+int time=18;
+if(time<10)
+{
+    Console.WriteLine("Good Moring");
+}
+else
+{
+    Console.WriteLine("Good Evening");
+}
+
+Console.WriteLine(10<9);
+
+int time2=11;
+switch(time2) 
+{
+  case 5:
+    // code block
+    Console.WriteLine("Good Morning");
+    break;
+  case 13:
+    // code block
+    Console.WriteLine("Good Afternoon");
+    break;
+    case 18:
+    // code block
+    Console.WriteLine("Good Evening");
+    break;
+    case 22:
+    // code block
+    Console.WriteLine("Good Night");
+    break;
+  default:
+    // code block
+    Console.WriteLine("Hi");
+    break;
+}
+
+int i=0;
+
+
+while(i<5)
+{
+Console.WriteLine(i);
+i=i+1;
+}
+
+for (int c = 0; c < 5; c++) 
+{
+  Console.WriteLine(c);
+} 
+
+Console.WriteLine("Break statement");
+
+for (int d = 0; d < 10; d++) 
+{
+  if (d == 4) 
+  {
+    break;
+  }
+  Console.WriteLine(d);
+}
+
+string [] cars={"volvo","BMW","ford"};
+Console.WriteLine(cars[2]);
+
+        // Console.WriteLine (C_Datatypes.X());
+    }
+}
